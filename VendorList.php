4efca52cb147c2699dcfd5cd7944e91e7f07e9f2
Dhruv Singhal <100797143+Dhruv-Singhal-15@users.noreply.github.com>
2023-08-20 18:16:21 +0530

    Add files via upload

---
+++ VendorList.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <style>
+        body{background-image: url(ii1.jpg);}.ratings i{font-size: 16px;color: red}.strike-text{color: red;text-decoration: line-through}.product-image{width: 100%}.dot{height: 7px;width: 7px;margin-left: 6px;margin-right: 6px;margin-top: 3px;background-color: blue;border-radius: 50%;display: inline-block}.spec-1{color: #938787;font-size: 15px}h5{font-weight: 400}.para{font-size: 16px}
+        </style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">EVENTRA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+        <a class="nav-link" href="eventra.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="login.php">Login<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Servies</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+</u1>
+</nav>
+<body>
+<div class="container mt-5 mb-5">
+    <div class="d-flex justify-content-center row">
+        <div class="col-md-10">
+            <div class="row p-2 bg-white border rounded">
+                <div class="col-md-3 mt-1"><img class="img-fluid img-responsive rounded product-image" src="ii3.jpg"></div>
+                <div class="col-md-6 mt-1">
+                    <h5>Fortune Five Star</h5>
+                    <div class="d-flex flex-row">
+                        <div class="ratings mr-2"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+                    </div>
+                    <div class="mt-1 mb-1 spec-1"><span>Capacity - 1300</span><span class="dot"></span><span>Indoor</span><span class="dot"></span><span>Fully Air Conditioned<br></span></div>
+                    <div class="mt-1 mb-1 spec-1"><span>Parking</span><span class="dot"></span><span>Power Backup</span><span class="dot"></span><span>Multipurpose Lawn<br></span></div>
+                    <p class="text-justify text-truncate para mb-0">A banquet hall that hosts approximately 1300 guests is an ideal place to conduct your events.<br><br></p>
+                </div>
+                <div class="align-items-center align-content-center col-md-3 border-left mt-1">
+                    <div class="d-flex flex-row align-items-center">
+                        <h4 class="mr-1">Rs.1500 per plate</h4><span class="strike-text">Rs.2000</span>
+                    </div>
+                    
+                    <div class="d-flex flex-column mt-4"><button class="btn btn-primary btn-sm" type="button">Details</button><button class="btn btn-outline-primary btn-sm mt-2" type="button">Add to wishlist</button></div>
+                </div>
+            </div>
+            <div class="row p-2 bg-white border rounded">
+                <div class="col-md-3 mt-1"><img class="img-fluid img-responsive rounded product-image" src="ii3.jpg"></div>
+                <div class="col-md-6 mt-1">
+                    <h5>Fortune Five Star</h5>
+                    <div class="d-flex flex-row">
+                        <div class="ratings mr-2"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+                    </div>
+                    <div class="mt-1 mb-1 spec-1"><span>Capacity - 1300</span><span class="dot"></span><span>Indoor</span><span class="dot"></span><span>Fully Air Conditioned<br></span></div>
+                    <div class="mt-1 mb-1 spec-1"><span>Parking</span><span class="dot"></span><span>Power Backup</span><span class="dot"></span><span>Multipurpose Lawn<br></span></div>
+                    <p class="text-justify text-truncate para mb-0">A banquet hall that hosts approximately 1300 guests is an ideal place to conduct your events.<br><br></p>
+                </div>
+                <div class="align-items-center align-content-center col-md-3 border-left mt-1">
+                    <div class="d-flex flex-row align-items-center">
+                        <h4 class="mr-1">Rs.1500 per plate</h4><span class="strike-text">Rs.2000</span>
+                    </div>
+                    
+                    <div class="d-flex flex-column mt-4"><button class="btn btn-primary btn-sm" type="button">Details</button><button class="btn btn-outline-primary btn-sm mt-2" type="button">Add to wishlist</button></div>
+                </div>
+            </div>
+            <div class="row p-2 bg-white border rounded">
+                <div class="col-md-3 mt-1"><img class="img-fluid img-responsive rounded product-image" src="ii3.jpg"></div>
+                <div class="col-md-6 mt-1">
+                    <h5>Fortune Five Star</h5>
+                    <div class="d-flex flex-row">
+                        <div class="ratings mr-2"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+                    </div>
+                    <div class="mt-1 mb-1 spec-1"><span>Capacity - 1300</span><span class="dot"></span><span>Indoor</span><span class="dot"></span><span>Fully Air Conditioned<br></span></div>
+                    <div class="mt-1 mb-1 spec-1"><span>Parking</span><span class="dot"></span><span>Power Backup</span><span class="dot"></span><span>Multipurpose Lawn<br></span></div>
+                    <p class="text-justify text-truncate para mb-0">A banquet hall that hosts approximately 1300 guests is an ideal place to conduct your events.<br><br></p>
+                </div>
+                <div class="align-items-center align-content-center col-md-3 border-left mt-1">
+                    <div class="d-flex flex-row align-items-center">
+                        <h4 class="mr-1">Rs.1500 per plate</h4><span class="strike-text">Rs.2000</span>
+                    </div>
+                    
+                    <div class="d-flex flex-column mt-4"><button class="btn btn-primary btn-sm" type="button">Details</button><button class="btn btn-outline-primary btn-sm mt-2" type="button">Add to wishlist</button></div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,160 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="project";
+
+$con=mysqli_connect($host,$user,$password,$db);
+
+if(isset($_GET['login']))
+{
+    $uid=$_GET['uid'];
+    $pass=$_GET['password'];
+
+    $sql1="select * from login where userid='".$uid."' and password='".$pass."'";
+
+    $result=mysqli_query($con,$sql1);
+    function Redirect($url, $permanent = false)
+{
+    header('Location: ' . $url, true, $permanent ? 301 : 302);
+
+    exit();
+}
+
+
+  session_start();
+  $_SESSION['userid']=$uid;
+  $_SESSION['bill']=0;
+
+
+    if(mysqli_num_rows($result) == 1)
+    {
+      Redirect('services.php', false);
+    }
+    else{echo("wrong!!");}
+}
+if(isset($_GET['register']))
+{
+    $uid=$_GET['uid'];
+    $pass=$_GET['password'];
+
+    
+    $sql2="insert into login values('".$uid."','".$pass."',0)";
+    $result=mysqli_query($con,$sql2);
+
+    
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<style>
+  body{padding: 0px;
+            margin: 0px;
+    background-image: url("blur.jpeg");
+background-repeat: no-repeat;
+background-size: 100%;}
+  .container{height: 500px;
+    width: 500px;
+    position:absolute;
+    top: 25%;
+    left: 15%;
+    border: 2px solid black;
+               border-radius: 5px;
+               
+               display: flex;
+               display: inline-block;
+               padding: 20px;}
+               .container p{
+                align-self: center;
+                align-items: center;
+                font-size: 25px;
+               }
+  #mail{margin: 5px;
+  border: 1px solid black;
+ font-size: 20px;
+ width: 100%;
+ }
+  #pass{margin: 5px;
+  border: 1px solid black;
+ font-size: 20px;
+ width: 100%;
+ }
+ #loginbut{width: 100%;
+  margin: 5px;
+  border: 1px solid black;
+ font-size: 20px;
+ }
+ #regbut{width: 100%;
+  margin: 5px;
+  border: 1px solid black;
+ font-size: 20px;
+ padding-left: 35%;
+ padding-right: 35%;}
+ p{text-align: center;}
+ #loginbut:hover{
+  background-color: rgb(205, 205, 205);
+ }
+ #regbut:hover{
+  background-color: rgb(205, 205, 205);
+ }
+</style>
+<body>
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="eventra.php">Eventra</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="eventra.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.html">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.php">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="aboutus.php">About Us</a>
+          </li>
+         
+         
+        </ul>
+        <form class="d-flex" role="search" method="get" action="login.php">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <p style="font-weight: bold;">Enter Login Details</p>
+    <div>
+      <form class="form" action="login.php" method="get">
+        <input id="mail" type="text" name="uid"  placeholder="user id"><br>
+        <input id="pass" type="text" name="password"  placeholder="password"><br>
+        <input id="loginbut" type="submit" name="login" value="Login"><br>
+        <p style="color:white;">Not registered?</p>
+        <input id="regbut" type="submit" name="register" value="Register">
+
+      </form>
+    </div>
+  </div>
+</body>
+</html>
